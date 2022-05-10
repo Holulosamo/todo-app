ef295ff515aacb304ff374b6154b24e12d346dba
Holulosamo <eliasrodriguez000@gmail.com>
2022-05-10 04:46:46 -0300

    dist build

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,41 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import ThemeButton from './components/ThemeButton'
-import TodoForm from './components/TodoForm'
-import TodoContainer from './components/TodoContainer'
+import { useEffect, useState } from 'react';
+import './App.css';
+import ThemeButton from './components/ThemeButton';
+import TodoForm from './components/TodoForm';
+import TodoContainer from './components/TodoContainer';
+
+const defaultValues = [
+  {
+    id: "1",
+    task: "Complete online JavaScript course",
+    done: true,
+  },
+  {
+    id: "2",
+    task: "Jag around the park 3x",
+    done: false,
+  },
+  {
+    id: "3",
+    task: "10 minutes meditation",
+    done: false,
+  },
+  {
+    id: "4",
+    task: "Read for 1 hour",
+    done: false,
+  },
+  {
+    id: "5",
+    task: "Pick up groceries",
+    done: false,
+  },
+  {
+    id: "6",
+    task: "Complete Todo App on Frontend Mentor",
+    done: false,
+  }
+];
 
 const FILTER_MAP = {
   All: () => true,
@@ -15,7 +48,7 @@ const FILTER_NAMES = Object.keys(FILTER_MAP);
 function App() {
   const [todo, setTodo] = useState(() => {
     const savedItems = JSON.parse(localStorage.getItem("savedTodo"));
-    return savedItems ? savedItems : []
+    return savedItems ? savedItems : defaultValues;
   });
   const [filter, setFilter] = useState("All");
   const [theme, setTheme] = useState(() => {
