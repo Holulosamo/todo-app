df94d13f48d3abea6bd355a9922a6b645efc61a3
Holulosamo <eliasrodriguez000@gmail.com>
2026-09-01 18:15:43 -0300

    Added AuthLayout

--- backend/connection.js
+++ backend/connection.js
@@ -1,1 +1,14 @@
-console.log("Connecting to the database...");
+import express from 'express';
+import pg from 'pg';
+
+const { Client } = pg;
+
+const connString = "postgresql://postgres:admin@localhost:5432/todo-db"
+
+const client = new Client(connString);
+
+console.log(client)
+
+client.connect()
+  .then(() => console.log('Connected to PostgreSQL database'))
+  .catch(err => console.error('Connection error', err.stack));
